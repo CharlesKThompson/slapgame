7cc1ebe5ec17ad55bcf2362548293874ddc762d7
Charles <chuckness32@hotmail.com>
2018-02-05 17:00:04 -0700

    sixth commit

--- assets/game.js
+++ assets/game.js
@@ -1,3 +1,4 @@
+
 var playerHealth = document.getElementById('health')
 var hitsElem = document.getElementById('hits')
 
@@ -9,13 +10,18 @@ var players = [
         name: "Gharnef",
         health: 100,
         hits: 0,
-        items:[]
+        items: []
     },
     {
         name: "Chrom",
         health: 100,
         hits: 0,
-        items:[]
+        items: [],
+        attacks: {
+            sword: 2,
+            lance: 4,
+            axe: 6,
+        }
     }]
 
 
@@ -35,31 +41,108 @@ function sword(i) {
 }
 
 function axe(i) {
-    players[i].health -= 5
+    players[i].health -= 6
     players[i].hits++
     draw(players)
 }
 
 function lance(i) {
-    players[i].health -= 3
+    players[i].health -= 4
     players[i].hits++
     draw(players)
 }
 
-var Item = function (name, properties, description){
+var Item = function (name, properties, description) {
     this.name = name
     this.properties = properties
     this.description = description
 }
 
 var items = {
-    shield:new Item("Shield",0.3,"This is an awesome shield!"),
-    potion:new Item("Gauntlet",15,"restorative panacea"),
-    banner:new Item("Banner",3.2,"Rally to the cause!")
+    shield: new Item("shield", 0.5, "This is an awesome shield!"),
+    gauntlet: new Item("gauntlet", 1.5, "restorative panacea"),
+    banner: new Item("banner", 2.0, "Rally to the cause!")
 }
 
+// function giveItems() {
+//     players[i].items.push(shield, gauntlet, banner)
+// }
 function giveShield(){
-    players[i].items.push(items.shield)
+    var item = items.shield
+    var player=players[0]
+    player.items.push(item)
 }
 
+function giveGauntlet(){
+    var item= items.gauntlet
+    var player=players[0]
+    player.items.push(item)
+}
+
+function giveBanner(){
+    var item = items.banner
+    var player=players[0]
+    player.items.push(item)
+}
+function addMods() {
+    var modTotal = 0
+
+    for (let i = 0; i < players.length; i++) {
+        modTotal += players.items[i].properties;
+        return modTotal
+
+    }
+
+
+}
+//function shield(i) {
+//     players[i].health -= damage *= 0.5
+
+// }
+
+/*
+var pizza = {
+    toppings: [toppings.pepperoni, toppings.cheese]
+}
+
+var toppingsList = {
+    pepperoni: {name: 'pepperoni'}
+    cheese: {name: 'cheese'}
+    anchovies: {name: 'anchovies'}
+    olives: {name: 'olives'}
+}
+
+var pizza ={
+    toppings: []
+}
+
+function addTopping(toppingName) {
+var topping = toppingsList[toppingName]
+if(topping){
+    pizza.toppings.push(topping)
+}else {
+    console.log('SORRY WE DONT HAVE THAT AS AN OPTION')
+    console.log('here is a list of the available toppings', toppingsList)
+}
+}
+
+addTopping('pepperoni')
+
+function totalKCals(pizza){
+    var totalKCals = 0
+    for (let i = 0; i < pizza.toppings.length; i++){
+        const topping = pizza.toppings[i]; *ALIAS
+        totalKCals += topping.kcal
+    }
+    return totalKCals
+}
+
+if(pizza.toppings.includes(topping)) { return }
+*is the same as*
+for (let i = 0; i < pizza.toppings.length; i++)
+const topping = pizza.toppings[i];
+if (topping.name == toppingName) { return }
+
+optional { return cosole.log('.....')}
+*/
 
